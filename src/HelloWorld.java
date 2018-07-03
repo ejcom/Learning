23fc8b57b15777cd45ffe4c7ace4672bbe7bad75
EJcom <ejikcom.eve@gmail.com>
2018-07-03 19:17:59 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a+b);
+        if (a>b)
+            System.out.println("A>B");
+        else
+            System.out.println("A<B");
+        System.err.println();
+    }
+}
