aeec2dd84b60e5c269cf6ce5c21635dba7e0ece5
EJcom <ejikcom.eve@gmail.com>
2018-07-03 22:42:52 +0200

    Обьденил все в один класс с функциями

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,22 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        System.out.println("If \nCase");
+        switch (in.nextLine())
+        {
+            case "If":
+                LearnIf();
+                break;
+            case "Case":
+                CaseMain();
+                break;
+        }
+    }
+
+    public static void LearnIf()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter A, B and program write result of IF ");
         int a = in.nextInt();
         int b = in.nextInt();
         System.out.println(a+b);
@@ -11,11 +27,23 @@ public class HelloWorld {
         else {
             System.out.println("A<B");
         }
+    }
 
-        if (a==1)
+    public static void CaseMain()
+    {
+        int dayCount;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter string and program write result Январь or not");
+        String month = in.nextLine();
+        switch (month)
+
         {
-            CaseLearn cln = new CaseLearn();
-            cln.CaseMain();
+            case "январь":
+                dayCount = 31;
+                System.out.println(dayCount);
+                break;
+            default:
+                System.out.println("Не январь");
         }
     }
 }
