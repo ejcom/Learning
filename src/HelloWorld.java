df1f662849ef8320bfb8529b08c10a8079bc0f4f
EJcom <ejikcom.eve@gmail.com>
2018-07-12 01:44:10 +0200

    12.07.2018

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,27 +3,33 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.println("1. If\n2. Case\n3. For\n4. While");
+        System.out.println("1. If\n2. Case\n3. For\n4. While\n5. Array");
         switch (in.nextLine())
         {
             case "1":
-                LnIf();
+                lnIf();
                 break;
             case "2":
-                LnCase();
+                lnCase();
                 break;
             case "3":
-                LnFor();
+                lnFor();
                 break;
             case "4":
-            LnWhile();
-            break;
+                lnWhile();
+                break;
+            case "5":
+                System.out.println("Array: Read & Write");
+                int[] array = lnReadArray();
+                System.out.println("Print your array? 1. Yes 2. No");
+                if(in.nextInt()==1) lnPrintArray(array);
+                break;
             default:
                 System.out.println("Error");
         }
     }
 
-    public static void LnIf()
+    public static void lnIf()
     {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter A, B and program write result of IF ");
@@ -37,7 +43,7 @@ public class HelloWorld {
         }
     }
 
-    public static void LnCase()
+    public static void lnCase()
     {
         int dayCount;
         Scanner in = new Scanner(System.in);
@@ -55,7 +61,7 @@ public class HelloWorld {
         }
     }
 
-    public static void LnFor()
+    public static void lnFor()
     {
         System.out.println("Enter A and Degree");
         Scanner in = new Scanner(System.in);
@@ -66,7 +72,7 @@ public class HelloWorld {
         System.out.println(a+"^"+b+"="+res);
     }
 
-    public static void LnWhile()
+    public static void lnWhile()
     {
         System.out.println("Enter A");
         Scanner in = new Scanner(System.in);
@@ -79,6 +85,22 @@ public class HelloWorld {
             c++;
         }
         System.out.println("2^"+c+" > "+a);
+    }
 
+    public static int[] lnReadArray()
+    {
+        int[] a;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter count");
+        int n = in.nextInt();
+        a = new int[n];
+        System.out.println("Enter elements "+n+" strings");
+        for (int i=0; i<n; i++) a[i] = in.nextInt();
+        return a;
+    }
+
+    public static void lnPrintArray(int[] a)
+    {
+       for(int i=0; i<a.length; i++) System.out.println(a[i]);
     }
 }
