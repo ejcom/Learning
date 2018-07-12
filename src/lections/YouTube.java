04cce64866f9e0d29b7769cb85b8f1114c3c72ec
EJcom <ejikcom.eve@gmail.com>
2018-07-12 21:27:25 +0200

    Bubble and inversion bubble

---
+++ src/lections/YouTube.java
@@ -0,0 +1,25 @@
+package lections;
+
+import java.util.Arrays;
+
+public class YouTube {
+    public static void main(String[] args) {
+        int[] data = {20, 50, 40, 10, 30};
+        for(int barrier = 0; barrier<=data.length-1; barrier++) {
+            for(int index = data.length-2; index>=barrier; index--) {
+                if(data[index]>data[index+1]) {
+                    swap(data, index);
+                }
+                System.out.println(Arrays.toString(data));
+            }
+        }
+        System.out.println(Arrays.toString(data));
+    }
+
+    private static void swap(int[] array, int k) {
+        int tmp = array[k];
+        array[k]= array[k + 1];
+        array[k + 1]=tmp;
+    }
+
+}
